446e64f7f2cfc1d99b3d00c831381d52e73b6ec3
Razar <marciorjuliao@gmail.com>
2023-12-06 17:11:33 -0300

    EstMVC Finalizada

--- Controllers/ItensController.cs
+++ Controllers/ItensController.cs
@@ -10,12 +10,19 @@ using System.Collections.Generic;
 using EstMVC.Models;
 using Newtonsoft.Json;
 using Microsoft.AspNetCore.Http;
+using System.Net.Http.Headers;
 
 namespace EstMVC.Controllers
 {
     public class ItensController : Controller
     {
         public string uriBase = "http://EstoqueHAS.somee.com/EstAPI/Itens/";
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
 
         [HttpGet]
         public async Task<ActionResult> IndexAsync()
@@ -25,14 +32,14 @@ namespace EstMVC.Controllers
                 string uriComplementar = "GetAll";
                 HttpClient httpClient = new HttpClient();
 
-                HttpResponseMessage response =  await httpClient.GetAsync(uriBase + uriComplementar);
+                HttpResponseMessage response = await httpClient.GetAsync(uriBase + uriComplementar);
                 string serialized = await response.Content.ReadAsStringAsync();
 
-                if  (response.StatusCode == System.Net.HttpStatusCode.OK)
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
                 {
                     List<ItemViewModel> listaItens = await Task.Run(() =>
-                    JsonConvert.DeserializeObject<List<ItemViewModel>>(serialized));           
-                    
+                    JsonConvert.DeserializeObject<List<ItemViewModel>>(serialized));
+
                     return View(listaItens);
                 }
                 else
@@ -43,7 +50,144 @@ namespace EstMVC.Controllers
                 TempData["MensagemErro"] = ex.Message;
                 return RedirectToAction("Index");
             }
-    }
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> DetailsAsync(int? id)
+        {
+            try
+            {
+                HttpClient httpClient = new HttpClient();
+
+                HttpResponseMessage response = await httpClient.GetAsync(uriBase + id.ToString());
+                string serialized = await response.Content.ReadAsStringAsync();
+
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    ItemViewModel p = await Task.Run(() =>
+                        JsonConvert.DeserializeObject<ItemViewModel>(serialized));
+                    TempData["Mensagem"] = string.Format("Item com Id {0} removido com sucesso!", id);
+                    return View(p);
+                }
+                else
+                    throw new System.Exception(serialized);
+            }
+            catch (System.Exception ex)
+            {
+                TempData["MensagemErro"] = ex.Message;
+                return RedirectToAction("Index");
+            }
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> EditAsync(int? id)
+        {
+            try
+            {
+                HttpClient httpClient = new HttpClient();
+
+                HttpResponseMessage response = await httpClient.GetAsync(uriBase + id.ToString());
+
+                string serialized = await response.Content.ReadAsStringAsync();
+
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    ItemViewModel p = await Task.Run(() =>
+                        JsonConvert.DeserializeObject<ItemViewModel>(serialized));
+                    return View(p);
+                }
+                else
+                    throw new System.Exception(serialized);
+            }
+            catch (System.Exception ex)
+            {
+                TempData["MensagemErro"] = ex.Message;
+                return RedirectToAction("Index");
+            }
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult> CreateAsync(ItemViewModel p)
+        {
+            try
+            {
+                HttpClient httpClient = new HttpClient();
+
+                var content = new StringContent(JsonConvert.SerializeObject(p));
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                HttpResponseMessage response = await httpClient.PostAsync(uriBase, content);
+                string serialized = await response.Content.ReadAsStringAsync();
+
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    TempData["Mensagem"] = string.Format("Item {0}, Id {1} salvo com sucesso!", p.Nome, serialized);
+                    return RedirectToAction("Index");
+                }
+                else
+                    throw new System.Exception(serialized);
+            }
+            catch (System.Exception ex)
+            {
+                TempData["MensagemErro"] = ex.Message;
+                return RedirectToAction("Index");
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> EditAsync(ItemViewModel p)
+        {
+            try
+            {
+                HttpClient httpClient = new HttpClient();
+
+                var content = new StringContent(JsonConvert.SerializeObject(p));
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+                HttpResponseMessage response = await httpClient.PutAsync(uriBase, content);
+                string serialized = await response.Content.ReadAsStringAsync();
+
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    TempData["Mensagem"] =
+                        string.Format("Item {0}, categoria {1} atualizado com sucesso!", p.Nome, p.Categoria);
+                    return RedirectToAction("Index");
+                }
+                else
+                    throw new System.Exception(serialized);
+            }
+            catch (System.Exception ex)
+            {
+                TempData["MensagemErro"] = ex.Message;
+                return RedirectToAction("Index");
+            }
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> DeleteAsync(int id)
+        {
+            try
+            {
+                HttpClient httpClient = new HttpClient();
+
+                HttpResponseMessage response = await httpClient.DeleteAsync(uriBase + id.ToString());
+                string serialized = await response.Content.ReadAsStringAsync();
+
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    TempData["Mensagem"] =
+                        string.Format("Item com ID: {0} removido com sucesso!", id);
+                    return RedirectToAction("Index");
+                }
+                else
+                    throw new System.Exception(serialized);
+            }
+            catch (System.Exception ex)
+            {
+                TempData["MensagemErro"] = ex.Message;
+                return RedirectToAction("Index");
+            }
+        }
 
 
     }
